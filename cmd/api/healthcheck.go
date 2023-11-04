829e11dd11524f76804cd1e68f4629d9f67cbacb
ukn0wme <ukn0wme.design@gmail.com>
2023-11-04 18:09:09 +0600

    Chapter 12.3 4 sec delay

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,9 +13,9 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 			"version":     version,
 		},
 	}
+	time.Sleep(4 * time.Second)
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		// Use the new serverErrorResponse() helper.
 		app.serverErrorResponse(w, r, err)
 	}
 }
--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
-	"os"        // New import
-	"os/signal" // New import
-	"syscall"   // New import
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,30 +19,32 @@ func (app *application) serve() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-	// Start a background goroutine.
+	shutdownError := make(chan error)
 	go func() {
-		// Create a quit channel which carries os.Signal values.
 		quit := make(chan os.Signal, 1)
-		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
-		// relay them to the quit channel. Any other signals will not be caught by
-		// signal.Notify() and will retain their default behavior.
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		// Read the signal from the quit channel. This code will block until a signal is
-		// received.
 		s := <-quit
-		// Log a message to say that the signal has been caught. Notice that we also
-		// call the String() method on the signal to get the signal name and include it
-		// in the log entry properties.
-		app.logger.PrintInfo("caught signal", map[string]string{
+		app.logger.PrintInfo("shutting down server", map[string]string{
 			"signal": s.String(),
 		})
-		// Exit the application with a 0 (success) status code.
-		os.Exit(0)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		shutdownError <- srv.Shutdown(ctx)
 	}()
-	// Start the server as normal.
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
 	})
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+	return nil
 }
