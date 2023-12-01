8c5dff7256e6ba052e9b503948db0dc3f7269b7c
ukn0wme <ukn0wme.design@gmail.com>
2023-12-01 18:04:57 +0600

    Chapter 13 finished

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,5 +18,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/songs/:id", app.updateSongHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/songs/:id", app.deleteSongHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
---
+++ cmd/api/users.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"nurgazinovd_golang_lg/internal/data"
+	"nurgazinovd_golang_lg/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Create an anonymous struct to hold the expected data from the request body.
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	// Parse the request body into the anonymous struct.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	v := validator.New()
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		// If we get a ErrDuplicateEmail error, use the v.AddError() method to manually
+		// add a message to the validator instance, and then call our
+		// failedValidationResponse() helper.
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Write a JSON response containing the user data along with a 201 Created status
+	// code.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
